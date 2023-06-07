a264b735d055eff4366f89b4bf002118eafe880a
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-06 20:29:08 -0600

    countries Service n interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     SharedModule
   ],
   providers: [],
