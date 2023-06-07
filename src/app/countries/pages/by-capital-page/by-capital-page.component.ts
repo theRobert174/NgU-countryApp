6090be384451f32acf8813b3bf3438f0d7fec73e
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-06 20:37:58 -0600

    show capitals

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'countries-by-capital-page',
@@ -7,8 +9,13 @@ import { Component } from '@angular/core';
   ]
 })
 export class ByCapitalPageComponent {
-  searchByCapital(term: string){
-    console.log('ByCapitalPage',{term});
 
+  public countries : Country[] = [];
+
+  constructor( private countriesService : CountriesService ) { }
+
+  searchByCapital(term: string){
+    // console.log('ByCapitalPage',{term});
+    this.countriesService.searchCapital(term).subscribe(countries => this.countries = countries)
   }
 }
