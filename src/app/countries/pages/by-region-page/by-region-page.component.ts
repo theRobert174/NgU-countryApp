afc6fc598b2effffca409a1cd0c6a57a2e4048e8
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-06-07 21:22:50 -0600

    by region optimization

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country.interface';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -12,13 +14,16 @@ export class ByRegionPageComponent {
 
   public countries : Country[] = [];
   public isLoading : boolean = false;
+  public regions: Region[] = ['Africa','Americas','Asia','Europe','Oceania'];
+  public selectedRegion?: Region;
 
   constructor( private countriesService: CountriesService ) {}
 
-  searchByRegion(term: string){
+  searchByRegion(region: Region){
     // console.log('ByCapitalPage',{term});
+    this.selectedRegion = region;
     this.isLoading = true;
-    this.countriesService.searchRegion(term).subscribe(countries => {
+    this.countriesService.searchRegion(region).subscribe(countries => {
       this.countries = countries;
       this.isLoading = false;
     });
